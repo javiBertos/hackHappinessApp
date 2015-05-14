498e3451a1b7c404b4f6465eec4e05f166bb4281
David Rodenas Pico <david.rodenas@gmail.com>
2015-05-14 22:00:01 +0200

    add happy-list

--- app/components/happies/happies.js
+++ app/components/happies/happies.js
@@ -3,4 +3,5 @@
 angular.module('myApp.happies', [
   'myApp.happies.happies-service',
   'myApp.happies.happy-add-form',
+  'myApp.happies.happy-list',
 ])
---
+++ app/components/happies/happy-list.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.happies.happy-list', [])
+
+// <happy-list></happy-list>
+.directive('happyList', function() {
+	var directive = {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'components/happies/happy-list.tpl.html' ,
+		controller: 'HappyListController',
+		controllerAs: 'vm',
+	};
+	return directive;
+})
+
+.controller('HappyListController', function(HappiesService) {
+	var vm = this;
+	vm.happies = HappiesService.list;
+});
